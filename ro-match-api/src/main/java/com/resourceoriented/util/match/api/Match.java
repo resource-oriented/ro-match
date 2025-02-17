7383851ac7bb6aec4108d8b4d08adb4f90b38a76
Russell Allen <Russell.Allen@netfoundry.io>
2025-02-17 18:56:59 -0500

    first commit

---
+++ ro-match-api/src/main/java/com/resourceoriented/util/match/api/Match.java
@@ -0,0 +1,11 @@
+package com.resourceoriented.util.match.api;
+
+/**
+ * {@link Match} provides a collection of static factory methods that application code can
+ * use to build a match expression.  These methods are meant to be composed as needed.
+ */
+public class Match {
+
+  private Match() {}
+
+}
